f0c8a9959d956362296236104d4fe5700e6bf423
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-06-20 14:22:50 -0500

    split files

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
 import { Url } from 'url';
+import { join } from 'path';
+
 const { ApolloServer, gql } = require('apollo-server');
 import { loadSchemaSync } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { addResolversToSchema } from '@graphql-tools/schema';
-import { join } from 'path';
 
-const schema = loadSchemaSync(join('src', './schemas/schema.gql'), { loaders: [new GraphQLFileLoader()] });
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: { name: string }) => `Hello, ${name}`,
-    post: (_: any, { id }: { id: string }) => ({
-      id: id,
-      title: 'the title',
-      content: 'the content'
-    })
-  }
-};
+import { post } from './resolvers/post';
+import { user } from './resolvers/user';
 
-const schemaWithResolvers = addResolversToSchema({
-  schema,
-  resolvers
-});
-
-const server = new ApolloServer({ schema: schemaWithResolvers });
+const schema = loadSchemaSync(join('src', './schemas/*.gql'), { loaders: [new GraphQLFileLoader()] });
+const server = new ApolloServer({ typeDefs: schema, resolvers: [post, user] });
 
 server.listen(9000).then(({ url }: { url: Url }) => console.log(`Server listening on ${url}`));
